a117266ac6a4b69ae0e1958486b9d4d94ff49ad5
gio87 <gio.dz@icloud.com>
2024-01-15 18:42:58 -0500

    add heders components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import { MyContext } from "./context";
 import ScrollToTop from "./utils/ScrollOnTop";
-export type MyContextProps = {};
+import Login from "./components/Login";
+export type MyContextProps = {
+  darkLight: boolean;
+  setDarkLight: (value: boolean) => void;
+  login: boolean;
+  setLogin: (value: boolean) => void;
+};
 function App() {
+  const [darkLight, setDarkLight] = useState<boolean>(true);
+  const [login, setLogin] = useState<boolean>(false);
   return (
-    <MyContext.Provider value={{}}>
-      <ScrollToTop />
+    <MyContext.Provider
+      value={{
+        darkLight,
+        setDarkLight,
+        login,
+        setLogin,
+      }}
+    >
+      {/* <ScrollToTop /> */}
       <Header />
+      {login ? <Login /> : ""}
     </MyContext.Provider>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,45 @@ import { useUserContext } from "../context";
 export default function Header() {
   const context = useUserContext();
   return (
-    <div className="flex flex-row items-center justify-between w-full px-[76] py-5">
-      <img src="./assets/LOGO-02 3.png" alt="" />
+    <header
+      className={` ${
+        context.darkLight
+          ? " bg-[#fff] text-[#1A1A1F]"
+          : "bg-[#050505] text-[#FFF]"
+      } flex flex-col items-center justify-between w-full md:h-20 px-5 pt-5 md:px-10 xl:px-[76px] border-b border-solid border-[#E4E3EB]`}
+    >
+      <div className=" flex flex-row items-center justify-between w-full">
+        <img
+          className="w-[80px] md:w-[150px]"
+          src="./assets/LOGO-02 3.png"
+          alt=""
+        />
 
-      <div className="bg-[#757575] w-10 h-5 rounded-xl relative ">
-        <button className="w-3.5 h-3.5  absolute rounded-full bg-white left-1 top-[3px]"></button>
+        <div className="flex flex-row items-center justify-between w-[130px] md:w-[180px]">
+          <div className={`bg-[#85858D] w-10 h-5 rounded-xl relative `}>
+            <button
+              onClick={() => {
+                context.setDarkLight(!context.darkLight);
+              }}
+              className={` ${
+                context.darkLight ? "left-1 top-[3px]" : " right-1 top-[3px]"
+              }  absolute  w-3.5 h-3.5  rounded-full bg-white  cursor-pointer`}
+            ></button>
+          </div>
+
+          <button
+            onClick={() => {
+              context.setLogin(!context.login);
+            }}
+            className={`px-4 py-2 bg-[#5D37f3] rounded-lg text-[12px] font-medium cursor-pointer text-[#fff]  md:px-5 md:py-2.5 md:text-[14px]`}
+          >
+            შესვლა
+          </button>
+        </div>
       </div>
-
-      <button className="px-10 py-5 bg-[#5D37f3] rounded-lg">შესვლა</button>
-    </div>
+      <div className="flex flex-row items-center justify-between w-full py-2 md:hidden">
+        <p>გრიგოლია</p> <p>შეგიძლია </p> <p>დასქრინო</p>
+      </div>
+    </header>
   );
 }
