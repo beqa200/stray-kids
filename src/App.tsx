72258b7759f73b10849d2b14446a5eb451d2aed9
gio87 <gio.dz@icloud.com>
2024-01-15 16:14:19 -0500

    add scrolltotop files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
 import { MyContext } from "./context";
-
+import ScrollToTop from "./utils/ScrollOnTop";
 export type MyContextProps = {};
 function App() {
   return (
     <MyContext.Provider value={{}}>
+      <ScrollToTop />
       <Header />
     </MyContext.Provider>
   );
