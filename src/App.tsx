81757eddd6f120f2e85924685977340a22c7945e
gio87 <gio.dz@icloud.com>
2024-01-15 16:09:10 -0500

    add context files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import { MyContext } from "./context";
 
+export type MyContextProps = {};
 function App() {
   return (
-    <>
+    <MyContext.Provider value={{}}>
       <Header />
-    </>
+    </MyContext.Provider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import { useUserContext } from "../context";
 
 export default function Header() {
+  const context = useUserContext();
   return (
-    <div>
+    <div className="flex flex-row items-center justify-between w-full px-[76] py-5">
       <img src="./assets/LOGO-02 3.png" alt="" />
 
-      <div>
-        <button></button>
+      <div className="bg-[#757575] w-10 h-5 rounded-xl relative ">
+        <button className="w-3.5 h-3.5  absolute rounded-full bg-white left-1 top-[3px]"></button>
       </div>
 
       <button className="px-10 py-5 bg-[#5D37f3] rounded-lg">შესვლა</button>
