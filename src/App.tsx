1cece7be51efec0b7543409d80003f8014dc3b09
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-16 02:33:45 +0400

    write routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AddBlog from "./pages/AddBlog";
+import SingleBlog from "./pages/SingleBlog";
+import ErrorPage from "./pages/ErrorPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<SingleBlog />} />
+        <Route path="/addBlog" element={<AddBlog />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
