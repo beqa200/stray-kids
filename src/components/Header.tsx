afcd88a6c1bba08f8151d4dac136159a61a19f47
gio87 <gio.dz@icloud.com>
2024-01-15 15:48:14 -0500

    add index.css files code

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <img src="./assets/LOGO-02 3.png" alt="" />
+
+      <div>
+        <button></button>
+      </div>
+
+      <button className="px-10 py-5 bg-[#5D37f3] rounded-lg">შესვლა</button>
+    </div>
+  );
+}
