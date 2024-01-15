4995a5ec0f978b277396a4718de3947c91ab5ca8
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-16 03:50:26 +0400

    import Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "@fontsource/firago/400.css";
+import "@fontsource/firago/500.css";
+import "@fontsource/firago/700.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
