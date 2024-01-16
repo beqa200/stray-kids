f5d89af356835c568d2a7edbd889cc0b12f88588
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-16 14:33:57 +0400

    almost done addBlog page UI

--- src/components/AddBlog/Inputs.tsx
+++ src/components/AddBlog/Inputs.tsx
@@ -43,7 +43,9 @@ const Inputs = () => {
       </div>
       {/* აღწერა */}
       <div className="flex flex-col space-y-2 mt-6">
-        <label className="text-[#1A1A1F]">აღწერა</label>
+        <label className="text-[#1A1A1F] text-sm font-medium leading-5">
+          აღწერა *
+        </label>
         <textarea
           className="min-h-[124px] bg-[#FCFCFD] border border-[#E4E3EB] rounded-xl pt-3 pl-4
           hover:border-[#5D37F3] hover:border-[1.5px] outline-none"
@@ -52,6 +54,46 @@ const Inputs = () => {
         <li className="text-[#85858D] text-xs leading-5">
           <span>მინიმუმ 2 სიმბოლო</span>
         </li>
+      </div>
+      {/*  */}
+      <div className="mt-6 flex flex-row justify-between">
+        <div className="flex flex-col space-y-2">
+          <label className="text-[#1A1A1F] text-sm font-medium leading-5">
+            გამოქვეყნების თარიღი *
+          </label>
+          <input
+            type="date"
+            className="w-[288px] py-3 px-4 rounded-xl border border-[#E4E3EB] bg-[#FCFCFD]
+            hover:border-[#5D37F3] hover:border-[1.5px] outline-none"
+          />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <label className="text-[#1A1A1F] text-sm font-medium leading-5">
+            კატეგორია *
+          </label>
+          <input
+            type="text"
+            placeholder="აირჩიეთ კატეგორია"
+            className="w-[288px] py-3 px-4 rounded-xl border border-[#E4E3EB] bg-[#FCFCFD]
+            hover:border-[#5D37F3] hover:border-[1.5px] outline-none"
+          />
+        </div>
+      </div>
+      {/*  */}
+      <div className="mt-6 flex flex-col space-y-2">
+        <label>ელ-ფოსტა</label>
+        <input
+          type="text"
+          placeholder="Example@redberry.ge"
+          className="w-[288px] py-3 px-4 rounded-xl border border-[#E4E3EB] bg-[#FCFCFD]
+            hover:border-[#5D37F3] hover:border-[1.5px] outline-none"
+        />
+      </div>
+      {/*  */}
+      <div className="my-10 flex justify-end">
+        <button className="text-white text-sm font-medium leading-5 py-[10px] w-[288px] bg-[#5D37F3] rounded-lg">
+          გამოქვეყნება
+        </button>
       </div>
     </div>
   );
